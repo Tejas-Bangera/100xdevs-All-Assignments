cee436313da6e66b603c522e6354b42ec61bfde5
TEJASBANGERA97 <tejas.bangera12@gmail.com>
2023-06-29 00:31:33 +0530

    Added User Routes to index.js

--- week-3/02-course-app-easy/index.js
+++ week-3/02-course-app-easy/index.js
@@ -23,23 +23,24 @@ app.post("/admin/signup", (req, res) => {
 });
 
 /**
- * __Function to validate login credentials__
+ * __Function to validate login credentials of an Admin/User__
  * @param {*} req
+ * @param {*} arr
  * @returns `true` if login credentials are valid else `false`
  */
-function validateLogin(req) {
+function validateLogin(req, arr) {
   const { username, password } = req.headers;
   // Validate login credentials
-  const admin = ADMINS.find((admin) => admin.username === username);
+  const user = arr.find((user) => user.username === username);
 
-  if (!admin || admin.password !== password) return false;
+  if (!user || user.password !== password) return;
 
-  return true;
+  return user;
 }
 
 app.post("/admin/login", (req, res) => {
   // logic to log in admin
-  if (!validateLogin(req))
+  if (!validateLogin(req, ADMINS))
     return res.status(401).json({ message: "Invalid login credentials" });
 
   res.json({ message: "Logged in successfully" });
@@ -48,7 +49,7 @@ app.post("/admin/login", (req, res) => {
 app.post("/admin/courses", (req, res) => {
   // logic to create a course
   // Validate session login credentials
-  if (!validateLogin(req))
+  if (!validateLogin(req, ADMINS))
     return res.status(401).json({ message: "Invalid login credentials" });
 
   const { title, description, price, imageLink, published } = req.body;
@@ -70,7 +71,7 @@ app.post("/admin/courses", (req, res) => {
 app.put("/admin/courses/:courseId", (req, res) => {
   // logic to edit a course
   // Validate session login credentials
-  if (!validateLogin(req))
+  if (!validateLogin(req, ADMINS))
     return res.status(401).json({ message: "Invalid login credentials" });
 
   const id = Number(req.params.courseId);
@@ -94,7 +95,7 @@ app.put("/admin/courses/:courseId", (req, res) => {
 app.get("/admin/courses", (req, res) => {
   // logic to get all courses
   // Validate session login credentials
-  if (!validateLogin(req))
+  if (!validateLogin(req, ADMINS))
     return res.status(401).json({ message: "Invalid login credentials" });
 
   res.json({ courses: COURSES });
@@ -103,22 +104,67 @@ app.get("/admin/courses", (req, res) => {
 // User routes
 app.post("/users/signup", (req, res) => {
   // logic to sign up user
+  let newUser = {
+    username: req.body.username,
+    password: req.body.password,
+    purchasedCourses: [],
+  };
+
+  USERS.push(newUser);
+
+  res.json({ message: "User created successfully" });
+  // res.status(201).json(newAdmin);
 });
 
 app.post("/users/login", (req, res) => {
   // logic to log in user
+  if (!validateLogin(req, USERS))
+    return res.status(401).json({ message: "Invalid login credentials" });
+
+  res.json({ message: "Logged in successfully" });
 });
 
 app.get("/users/courses", (req, res) => {
   // logic to list all courses
+  // Validate session login credentials
+  if (!validateLogin(req, USERS))
+    return res.status(401).json({ message: "Invalid login credentials" });
+
+  res.json({ courses: COURSES });
 });
 
 app.post("/users/courses/:courseId", (req, res) => {
   // logic to purchase a course
+  // Validate session login credentials
+  const user = validateLogin(req, USERS);
+
+  if (!user)
+    return res.status(401).json({ message: "Invalid login credentials" });
+
+  const id = Number(req.params.courseId);
+
+  const courseIndex = COURSES.findIndex((course) => course.id === id);
+
+  if (courseIndex === -1)
+    return res.status(404).json({ message: `Course with id ${id} not found!` });
+
+  user.purchasedCourses.push(id);
+  res.json({ message: "Course purchased successfully" });
 });
 
 app.get("/users/purchasedCourses", (req, res) => {
   // logic to view purchased courses
+  // Validate session login credentials
+  const user = validateLogin(req, USERS);
+
+  if (!user)
+    return res.status(401).json({ message: "Invalid login credentials" });
+
+  res.json({
+    purchasedCourses: user.purchasedCourses.map((id) =>
+      COURSES.find((course) => course.id === id)
+    ),
+  });
 });
 
 app.listen(3000, () => {
